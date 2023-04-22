05ac12553c36e5446d51fe15de599ea0b7c3b78d
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-22 13:03:13 -0700

    task 12..

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,20 +9,23 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 99; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = a; b <= 99; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
 			putchar(a / 10 + '0');
 			putchar(a % 10 + '0');
 			putchar(' ');
 			putchar(b / 10 + '0');
 			putchar(b % 10 + '0');
-			if (a != 98 || b != 99)
+
+			if (a == 98 && b == 99)
 			{
+				continue;
+			}
+
 			putchar(',');
 			putchar(' ');
-			}
 		}
 	}
 	putchar('\n');
