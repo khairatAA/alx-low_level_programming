3da3e705c77f067ae6720ec621a1f4e13d83a22d
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-21 22:52:40 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	char alp;
 
-	for (alp = 'a'; alp < 'z'; alp++)
+	for (alp = 'a'; alp <= 'z'; alp++)
 	{
-		if (alp < 'e' && alp > 'q')
+		if (alp == 'e' && alp == 'q')
 		{
 			continue;
 		}
