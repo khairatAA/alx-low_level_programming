5d5085fa5bbc628fae5d8817f1e754e7b1b7f475
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-21 22:42:34 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp < 'z'; alp++)
+	{
+		if (alp != 'e' && alp != 'q')
+		{
+			continue;
+		}
+	putchar(alp);
+	}
+	putchar('\n');
+
+	return (0);
+}
