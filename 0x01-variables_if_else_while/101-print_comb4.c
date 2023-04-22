65b3cc6ed6afffd9e6b05118dacc56d3dcbb3272
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-22 10:30:19 -0700

    task 10

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int x;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			for (x = j + 1; x <= 57; x++)
+			putchar(i);
+			putchar(j);
+			purchar(x);
+			if (i != 55 || j != 56 || x != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
