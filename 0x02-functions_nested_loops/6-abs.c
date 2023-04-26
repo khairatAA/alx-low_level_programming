5a099b3028319ba485629a9a7cf891ecb9d3be10
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-26 03:26:47 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @a: the integer valuse to test
+ *
+ * Return: Always 0 (success) in _abs
+ */
+
+int _abs(int a)
+{
+	a = abs(a);
+
+	return (0);
+}
