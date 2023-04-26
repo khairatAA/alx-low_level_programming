e11445544507111efa41cc978873d5715da65fa6
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-26 09:14:43 -0700

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 23; num1++)
+	{
+		for (num2 = 0; num2 <= 60; num2++)
+		{
+			_putchar(num1 / 10 + '0');
+			_putchar(num2 % 10 + '0');
+			_putchar(':');
+			_putchar(num2 / 10 + '0');
+			_putchar(num2 % 10 + '0');
+			_putchar('\n');
+
+			if (num1 == 23 && num2 == 59)
+			{
+				break;
+			}
+		}
+	}
+}
