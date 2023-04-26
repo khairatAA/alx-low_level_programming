055fbb1f7a1e531c84c64a21e7dbde3833bfb627
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-25 17:05:15 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
