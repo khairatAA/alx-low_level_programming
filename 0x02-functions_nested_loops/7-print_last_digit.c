c30a34a6134099a40a3fbdbed9119362c110cc39
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-26 07:05:38 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  prints the last digit of a number
+ *
+ * @i: integer to be tested
+ *
+ * Return: Always 0 (success)in print_last_digit
+ */
+
+int print_last_digit(int i)
+{
+	int a;
+
+	a = i % 10;
+
+	return (a);
+
+	return (0);
+}
