4be904e5f520774fef0c62574abf0e8f24f742f1
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-27 12:21:19 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square -  prints a square
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
+
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
