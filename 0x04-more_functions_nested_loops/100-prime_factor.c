11b822fa2f7d8c84568019c736ba6da6bc604e10
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-27 16:13:25 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+
+/**
+ * main - Entry point
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int max;
+	long n;
+
+	n = 612852475143;
+	max = 0;
+	while(n  % 2 == 0)
+	{
+		max = 2;
+		n = n / 2;
+	}
+	for(i = 3; i * i <= n; i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	{
+		max = n;
+	}
+	printf("%ld\n", n);
+
+	return (0);
+}
