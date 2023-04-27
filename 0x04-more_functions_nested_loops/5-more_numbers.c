ec9269d6ac7a4b87739de148adfde0060f2db122
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-27 10:15:50 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,14 @@ void more_numbers(void)
 {
 	int a, i;
 
-	a = 0;
+	a = 1;
 	while (a <= 10)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i + '0');
+			if (i >= 10)
+				_putchar('1');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 		a++;
