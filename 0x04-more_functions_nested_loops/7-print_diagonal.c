86e0fcdead459781fce739c02ebcb37696d25ae1
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-27 12:05:48 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,12 +13,16 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		int i;
+		int i, j;
 
 		for (i = 0; i < n; i++)
 		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
-		}
 			_putchar('\n');
+		}
 	}
 }
