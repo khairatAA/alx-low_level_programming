9af1e52d8b6774307d07a650ef0a40d0fab2c131
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-28 04:10:53 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,13 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if (k <= 9)
+			if (j != 0 && k <= 9)
 			{
 				_putchar(' ');
+				_putchar((k % 10) + '0');
+			}
+			else if (j == 0)
+			{
 				_putchar((k % 10) + '0');
 			}
 			else
