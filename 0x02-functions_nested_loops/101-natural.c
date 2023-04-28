8c6b091fc0fc6fb930fc808ff994d919210defc5
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-28 06:29:34 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,20 +7,15 @@
 int main(void)
 {
 	int i;
-	int sum3 = 0;
-	int sum5 = 0;
+	int sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			sum3 = sum3 + i;
-		}
-		if (i % 5 == 0)
-		{
-			sum5 = sum5 + i;
+			sum = sum + i;
 		}
 	}
-	printf("%d\n", sum3 + sum5);
+	printf("%d\n", sum);
 	return (0);
 }
