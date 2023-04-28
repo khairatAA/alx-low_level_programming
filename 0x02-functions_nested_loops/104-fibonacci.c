9eefad4f5e2f0796f9d8aaa6d8a7c80612bf31c5
Adesina Khairat <khairatadesina01@gmail.com>
2023-04-28 09:03:42 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	long a, b, c;
+
+	a = 1;
+	b = 2;
+	printf("%ld, %ld", a, b);
+	for (i = 3; i <= 98; i++)
+	{
+		c = a + b;
+		a = b;
+		c = b;
+	}
+	printf(" ,%ld", c);
+	printf("\n");
+
+	return (0);
+}
