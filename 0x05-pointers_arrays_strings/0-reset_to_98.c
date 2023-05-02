70b38b21d5e2100e0f15ca5c2011581b650f248d
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-02 16:28:49 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates
+ * @n: pointer to be tested
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
