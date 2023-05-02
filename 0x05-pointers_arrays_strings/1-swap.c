6435372d6634ebf5927470f968b96e8bd599de45
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-02 16:41:02 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of two integer
+ * @a: pointer to be tested
+ * @b: pointer to be tested
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
