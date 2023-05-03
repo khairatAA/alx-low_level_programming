1c64a700233faff7760ab42ecb197b0d0c2540a8
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-03 09:51:14 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,9 @@ void puts2(char *str)
 	int len;
 
 	len = strlen(str);
-	for (i = 0; i < len - 1; i += 2)
+	for (i = 0; i < len; i += 2)
 	{
+		if (len != '\0')
 		_putchar(str[i]);
 	}
 	_putchar('\n');
