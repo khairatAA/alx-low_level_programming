2c01a15e9b351742741bf68fb4d110d8b753846c
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-03 07:40:36 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,15 @@
  */
 void rev_string(char *s)
 {
-	int len = strlen(s);
-	int i;
-	char temp;
+	int len, i;
+	char ch;
 
+	len = strlen(s);
 	for (i = 0; i < len / 2; i++)
 	{
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		ch = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] =  ch;
 	}
 	puts(s);
 }
