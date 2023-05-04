af4855f79d4b65ee803075aa0768e25a108348d7
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-04 13:03:25 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to be test
+ * Return: success
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			continue;
+		}
+		if (str[i] == ' ')
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		if (str[i - 1] == '\t' || str[i - 1] == '\n')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			continue;
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 32;
+			}
+		}
+	}
+
+	return (str);
+}
