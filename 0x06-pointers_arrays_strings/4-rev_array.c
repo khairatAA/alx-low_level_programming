f511547d10f94728559e393888f9c63d7caafff6
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-04 06:48:43 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array
+ * @a: array to be tested
+ * @n: integer
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n; i >= n; i--)
+	{
+		_putchar(a[i]);
+	}
+}
