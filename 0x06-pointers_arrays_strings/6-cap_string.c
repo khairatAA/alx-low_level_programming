b5e57554b95ac7709b96fab59bca987d74b730f6
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-05 03:46:01 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,24 +13,23 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
-			|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"' ||
-			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (i == 0 ||
+			str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '\"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
 			str[i - 1] == '}')
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
 				str[i] = str[i] - 32;
-			}
-		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] = str[i] + 32;
-			}
-		}
 	}
 
 	return (str);
