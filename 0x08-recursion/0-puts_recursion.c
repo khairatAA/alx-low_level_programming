9332fdaaadf1f997fb78df5405c21997ca5dca61
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-10 07:07:23 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be tested
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
