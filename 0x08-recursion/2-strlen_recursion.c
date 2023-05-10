60fcbe2a7c9eaa16dee5a8a1051e7cdeb964d104
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-10 07:46:52 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be tested
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+}
