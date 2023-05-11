f6a0f16df2568c84682ead584228181392cff0cc
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-11 09:10:27 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,39 +3,43 @@
 #include <math.h>
 
 /**
- * _sqrt_recursion -  returns the natural square root of a number
+ * _sqrt_num - returns the natural square root of a number
  * @n: integer
  * @min: minimum
  * @max: maxximum
  * Return: n
  */
-int _sqrt_num(int n,int min, int max);
-
-int _sqrt_num(int n,int min, int max)
+int _sqrt_num(int n, int min, int max)
 {
 	int guess;
-	long int square;
 
 	if (max < min)
 
 		return (-1);
 	guess = (min + max) / 2;
-	square = guess * guess;
-	if (square == n)
+	if (guess == n / guess && n % guess == 0)
 
 		return (guess);
-	else if (square < n)
+	else if (guess < n / guess)
 
 		return (_sqrt_num(n, guess + 1, max));
 
-	return(_sqrt_num(n, min, guess - 1));
+	return (_sqrt_num(n, min, guess - 1));
 }
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ * Return: n
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 
 		return (-1);
+	if (n == 0 || n == 1)
 
-	return (_sqrt_num(n, 0, n));
+		return n;
+
+	return (_sqrt_num(n, 1, n));
 }
