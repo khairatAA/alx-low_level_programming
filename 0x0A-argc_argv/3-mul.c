725ec1fcfd7979f802a427d094d0833d26c2fafd
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-12 10:42:16 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,51 @@
 #include <stdio.h>
 #include <string.h>
-
+#include "main.h"
 /**
  * main - Entry point
- * Return: 1 success always
+ * @argc: command line arugments counts
+ * @argv: command line arugments string
+ * Return: 1 or 0 success always
  */
-int main(void)
+int _atoi(char *s);
+int main(int argc, char *argv[])
 {
-	int a, b, multi;
+	int a = 0, b = 0, multi;
 
-	scanf("%d", &a);
-	scanf("%d", &b);
-	multi = a * b;
-	if (a && b)
-		printf("%d\n", multi);
-	else
+	if (argc < 3 || argc > 3)
+	{
 		printf("Error\n");
 
-	return (1);
+		return (1);
+	}
+	a = _atoi(argv[1]);
+	b = _atoi(argv[2]);
+	multi = a * b;
+	printf("%d\n", multi);
+
+	return (0);
+}
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to test
+ * Return: success
+ */
+int _atoi(char *s)
+{
+	unsigned int value = 0;
+	int sign = 1;
+
+	do
+	{
+		if (*s == '-')
+			sign = sign * -1;
+		else if (*s >= '0' && *s <= '9')
+			value = (value * 10) + (*s - '0');
+		else if (value > 0)
+			break;
+	}
+	while (*s++);
+
+	return (value * sign);
 }
