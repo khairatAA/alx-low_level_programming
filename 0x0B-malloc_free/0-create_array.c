1f73ffd4091f08193dd95d12402c2e96a273b597
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-17 04:35:18 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,15 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptrArray;
 	unsigned int count;
+	if (size == 0)
+		return (NULL);
 
 	ptrArray = (char *)malloc(size * sizeof(char));
+	if (ptrArray == NULL)
+		return NULL;
 	for (count = 0; count < size; count++)
 	{
 		ptrArray[count] = c;
-	}
-	if (NULL)
-	{
-		printf("failed to allocate memory\n");
 	}
 
 	return (ptrArray);
