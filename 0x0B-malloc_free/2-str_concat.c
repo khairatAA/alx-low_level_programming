8cf4e78116dedd51934ab09c519bb3893622a789
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-18 05:19:20 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,20 +10,19 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int len1, len2;
-	char *ptrStr, result;
+	char *ptrStr;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	len1 = 0;
-	while (len1 != '\0')
+	while (s1[len1] != '\0')
 		len1++;
 	len2 = 0;
-	while (len2 != '\0')
+	while (s2[len2] != '\0')
 		len2++;
-	result = len1 + len2 + 1;
-	ptrStr = (char *)malloc((result * sizeof(char)));
+	ptrStr = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptrStr == NULL)
 
 		return (NULL);
