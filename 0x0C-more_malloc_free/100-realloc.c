1ff7f67b6ba4bc280518defe95a5ebeffa36da4e
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-19 05:38:29 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *newPtr;
 	unsigned int size;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
