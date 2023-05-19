6336920384265b09879e948648aaa22b96e73546
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-19 05:00:15 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	newPtr = malloc(new_size);
-	if (new_size)
+	if (newPtr)
 	{
 		size = (old_size < new_size) ? old_size : new_size;
 		memcpy(newPtr, ptr, size);
