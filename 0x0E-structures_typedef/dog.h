c67abaa4ba08b8012d3fa268a843ad8e69fc8a46
Adesina Khairat <khairatadesina01@gmail.com>
2023-05-22 10:26:04 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - contains details of a dog
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
