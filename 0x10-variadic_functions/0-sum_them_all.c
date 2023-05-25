8405de0f377c745cc23a333ec4ffbd90f9f696a9
khairatAA <khairatadesina01@gmail.com>
2023-05-25 22:26:54 +0100

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,7 +2,7 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all -  returns the sum of all its parameters
+ * sum_them_all - returns the sum of all its parameters
  * @n: number of arguments
  * @...: variadic function
  *
