947b958f4cd364b9266b1141acd90131f0807dd7
Adesina Khairat <khairatadesina01@gmail.com>
2023-06-01 13:28:48 -0700

    task 2

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: head
+ * @str: string
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newNode = (list_t *)malloc(sizeof(list_t));
+	list_t *temp;
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	newNode->len = strlen(str);
+	newNode->next = NULL;
+
+	if (*head == NULL)
+		*head = newNode;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = newNode;
+	}
+	
+	return (newNode);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /* Data structure to be used */
 /**
  * struct list_s - singly linked list
@@ -20,5 +25,6 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t list_len(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
