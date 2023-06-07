f40cb28e81c1b9ff09af5ddfe875cac54aef3097
Adesina Khairat <khairatadesina01@gmail.com>
2023-06-07 01:35:38 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,35 +8,25 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *temp = head;
+	const listint_t *temp;
 	const listint_t *currentNode = head;
 	size_t count = 0;
-	int loop = 0;
 
 	if (head == NULL)
 		exit(98);
-	while (temp != NULL && currentNode != NULL && temp->next != NULL)
-	{
-		printf("[%9p] %d\n", (void *)temp, temp->n);
-		currentNode = currentNode->next;
-		temp = temp->next;
-		if (currentNode == temp)
-		{
-			loop = 1;
-			break;
-		}
-		count++;
-	}
-	if (loop)
-	{
-		printf("[%9p] %d\n", (void *)currentNode, currentNode->n);
-		exit(98);
-	}
 	while (currentNode != NULL)
 	{
-		printf("[%9p] %d\n", (void *)currentNode, currentNode->n);
-		currentNode = currentNode->next;
+		printf("[%p] %d\n", (void *)currentNode, currentNode->n);
 		count++;
+		temp = currentNode;
+		currentNode = currentNode->next;
+		if (temp <= currentNode)
+		{
+			printf("-> [%p] %d\n", (void *)currentNode,
+					currentNode->n);
+			/* printf("-> [%p] %d\n", (void *)temp, temp->n); */
+			exit(98);
+		}
 	}
 
 	return (count);
