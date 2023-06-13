b8d577cf08236a4469d98bcf25787097b34b2e7f
Adesina Khairat <khairatadesina01@gmail.com>
2023-06-13 14:34:07 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,8 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (fp == NULL)
 			return (-1);
 		fprintf(fp, "%s", text_content);
+		fclose(fp);
 	}
-	fclose(fp);
 
 	return (1);
 }
