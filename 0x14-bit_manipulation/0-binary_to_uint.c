0e413cf4ec0653c45a7d2e0de48f3189808ddcb0
Khairat Adesina <khairatadesina01@gmail.com>
2023-07-07 16:57:42 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: num er to be converted
  *
+ * Return: the decimal number
  */
 unsigned int binary_to_uint(const char *b)
 {
