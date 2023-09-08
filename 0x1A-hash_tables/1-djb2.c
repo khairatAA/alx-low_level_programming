32a9e154e7a31d2dc80a56ae4c76a095b623023f
khairatAA <khairatadesina01@gmail.com>
2023-09-08 09:57:26 +0100

    task 1 betty

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,14 +1,16 @@
 #include "hash_tables.h"
 
 /**
- *
+ * hash_djb2 - the hash function that returns the index
+ * @str: the string who index is to be generated
+ * Return: the index
  */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
-	
+
 	hash = 5381;
 	while ((c = *str++))
 	{
